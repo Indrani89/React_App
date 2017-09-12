34ab6a8c6c8b4c7abb7958643c30eaae87749441
Indrani <indu_psg2008@yahoo.com>
2017-09-12 13:44:43 +1200

    Simple React Application performing CURD operation at component level

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//let message="HEllo from entry!!!";
+//console.log(message);
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import App from 'components/app';
+
+render(
+	<App />,
+	document.getElementById('app')
+);
